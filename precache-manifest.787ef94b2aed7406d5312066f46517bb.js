cff353cb9a5e44c12871fe43ea0ce686273a14c9
Shane Keney <shane@spikeball.com>
2020-04-18 00:41:46 -0400

    Updates

---
+++ precache-manifest.787ef94b2aed7406d5312066f46517bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d029a3dac824fcf1809a78f9983c9ce5",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "56ce8323aa0e5c1d8fc1",
+    "url": "/react-portfolio/static/css/main.18fddc18.chunk.css"
+  },
+  {
+    "revision": "4a10d6e04e9efb2d7da9",
+    "url": "/react-portfolio/static/js/2.36f6e2c1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.36f6e2c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56ce8323aa0e5c1d8fc1",
+    "url": "/react-portfolio/static/js/main.9f435389.chunk.js"
+  },
+  {
+    "revision": "c01136dc6b7e393f307b",
+    "url": "/react-portfolio/static/js/runtime-main.d92c7dfa.js"
+  }
+]);
